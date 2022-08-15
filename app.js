28ef5eec93cc48d4283f4ee85c8fea55a3d880a8
g3th3x <g3th3x@gmail.com>
2022-08-15 03:03:53 +0300

    working more

--- app.js
+++ app.js
@@ -9,20 +9,32 @@ window.addEventListener("load", () => {
   let requestId;
   let score = 0;
   let direction;
+  let isPause = true;
 
-  let earthWorm = [];
+  let worm = [];
+
+  document.addEventListener("keydown", (event) => {
+    const code = event.code;
+    if (code === "ArrowUp" && direction != "up") direction = "up";
+    else if (code === "ArrowDown" && direction != "down") direction = "down";
+    else if (code === "ArrowLeft" && direction != "right") direction = "left";
+    else if (code === "ArrowRight" && direction != "left") direction = "right";
+    if (code === "KeyP" && isPause) {
+      isPause = false;
+      cancelAnimationFrame(requestId);
+      gamePause();
+    } else if (code === "KeyP" && !isPause) {
+      isPause = true;
+      requestId = requestAnimationFrame(animate);
+    }
+  });
+
   //Стартовая позиция
-  earthWorm[0] = {
+  worm[0] = {
     x: 9 * cell,
     y: 10 * cell,
   };
-  //console.log(earthWorm[0].x, earthWorm[0].y);
-
-  function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются
-  }
+  //console.log(worm[0]);
 
   let spawnFood = {
     x: getRandomIntInclusive(1, 18) * cell,
@@ -31,15 +43,24 @@ window.addEventListener("load", () => {
   //console.log(`x: ${spawnFood.x}, y: ${spawnFood.y}`);
 
   // Основной игровой цикл
-  (function animate() {
+  function animate() {
     update();
     render();
-    requestAnimationFrame(animate);
-  })();
+    requestId = requestAnimationFrame(animate);
+  }
 
-  function update() {}
-  function render() {
+  function update() {
     //ctx.clearRect(0, 0, canvas.width, canvas.height);
+    let addHead = {
+      x: worm[0].x,
+      y: worm[0].y,
+    };
+
+    eatingTail(addHead, worm);
+    worm.unshift(addHead);
+  }
+
+  function render() {
     //background
     ctx.fillStyle = "#8B4513";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -65,10 +86,10 @@ window.addEventListener("load", () => {
     ctx.font = "42px Helvetica";
     ctx.fillText(`Your score: ${score}`, cell, cell * 1.3);
 
-    //earthWorm
-    for (let i = 0; i < earthWorm.length; i++) {
+    //worm
+    for (let i = 0; i < worm.length; i++) {
       ctx.fillStyle = i == 0 ? "#00ff00" : "#ff0000";
-      ctx.fillRect(earthWorm[i].x, earthWorm[i].y, cell, cell);
+      ctx.fillRect(worm[i].x, worm[i].y, cell, cell);
     }
 
     //food
@@ -78,127 +99,67 @@ window.addEventListener("load", () => {
     //   ctx.fill();
     ctx.fillRect(spawnFood.x, spawnFood.y, cell, cell);
 
-    let earthWormX = earthWorm[0].x;
-    let earthWormY = earthWorm[0].y;
-    //   console.log(
-    //     `earthWorm[0].x: ${earthWorm[0].x}, earthWorm[0].y: ${earthWorm[0].y}`
-    //   );
-    console.log(`earthWormX: ${earthWormX}, earthWormY: ${earthWormY}`);
-
     //collision - eating food
-    //   if (earthWormX === spawnFood.x && earthWormY === spawnFood.y) {
-    //     score++;
-    //     spawnFood = {
-    //       x: getRandomIntInclusive(1, 18) * cell,
-    //       y: getRandomIntInclusive(2, 18) * cell,
-    //     };
-    //   } else {
-    //     earthWorm.pop();
-    //   }
+    if (worm[0].x === spawnFood.x && worm[0].y === spawnFood.y) {
+      score++;
+      spawnFood = {
+        x: getRandomIntInclusive(1, 18) * cell,
+        y: getRandomIntInclusive(2, 18) * cell,
+      };
+    } else {
+      worm.pop();
+    }
 
     //moving
-    if (direction == "left") earthWormX -= cell;
-    if (direction == "right") earthWormX += cell;
-    if (direction == "up") earthWormY -= cell;
-    if (direction == "down") earthWormY += cell;
-    //   console.log(
-    //     `Direction: ${direction}, earthWormX: ${earthWormX}, earthWormY: ${earthWormY}`
-    //   );
+    if (direction === "left") worm[0].x -= cell;
+    if (direction === "right") worm[0].x += cell;
+    if (direction === "up") worm[0].y -= cell;
+    if (direction === "down") worm[0].y += cell;
 
-    requestId = requestAnimationFrame(render);
+    if (
+      worm[0].x < cell ||
+      worm[0].x > cell * 17 ||
+      worm[0].y < 3 * cell ||
+      worm[0].y > cell * 17
+    )
+      gameOver();
   }
 
-  //animate();
-
-  document.addEventListener("keydown", (event) => {
-    const key = event.code;
-    if (key === "ArrowUp" && direction != "up") direction = "up";
-    else if (key === "ArrowDown" && direction != "down") direction = "down";
-    else if (key === "ArrowLeft" && direction != "left") direction = "left";
-    else if (key === "ArrowRight" && direction != "right") direction = "right";
-    //console.log(key);
-  });
-
+  //self eating
   function eatingTail(head, arr) {
     for (let i = 0; i < arr.length; i++) {
       if (head.x == arr[i].x && head.y == arr[i].y) gameOver();
     }
   }
 
-  //cancelAnimationFrame(requestId);
+  function gameOver() {
+    //ctx.fillStyle = "#000";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
+    //ctx.globalAlpha = 0.5;
+    ctx.fillRect(128, 160, 384, 192);
+    //ctx.globalAlpha = 1;
+    ctx.fillStyle = "#800000";
+    ctx.font = "60px Helvetica";
+    ctx.fillText(`Game over!`, 160, 280);
+    cancelAnimationFrame(requestId);
+  }
 
-  // class Food {
-  //   constructor() {
-  //     // (this.x = Math.floor(Math.random() * 17 + 1) * box),
-  //     //   (this.y = Math.floor(Math.random() * 15 + 3) * box);
-  //   }
-  //   update() {
-  //     this.x = Math.floor(Math.random() * 17 + 1) * box;
-  //     this.y = Math.floor(Math.random() * 15 + 3) * box;
-  //   }
-  //     draw() { }
+  function gamePause() {
+    //ctx.fillStyle = "#000";
+    ctx.fillStyle = "rgba(0, 0, 0, 0.4)";
+    ctx.fillRect(128, 160, 384, 192);
+    ctx.fillStyle = "#800000";
+    ctx.font = "60px Helvetica";
+    ctx.fillText(`Pause`, 236, 280);
+    cancelAnimationFrame(requestId);
+  }
 
-  //     ctx.drawImage(
-  //       enemyImage,
-  //       this.frameX * this.spriteWidth,
-  //       this.frameY * this.spriteHeight,
-  //       this.spriteWidth,
-  //       this.spriteHeight,
-  //       this.x - 60,
-  //       this.y - 70,
-  //       this.spriteWidth / 3,
-  //       this.spriteHeight / 3
-  //     );
+  //Диапозон случайных чисел (max и min включаются)
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 
-  // }
-
-  // new Food(1);
-  // new Food(2);
-
-  // function eatSelfTail(head, arr) {
-  //   for (let i = 0; i < arr.length; i++) {
-  //     if (head.x == arr[i].x && head.y == arr[i].y) gameOver();
-  //   }
-  // }
-
-  // function draw() {
-
-  //   if (snakeX == food.x && snakeY == food.y) {
-  //     score++;
-  //     food = {
-  //       x: Math.floor(Math.random() * 17 + 1) * box,
-  //       y: Math.floor(Math.random() * 15 + 3) * box,
-  //     };
-  //   } else {
-  //     snake.pop();
-  //   }
-
-  //   if (
-  //     snakeX < box ||
-  //     snakeX > box * 17 ||
-  //     snakeY < 3 * box ||
-  //     snakeY > box * 17
-  //   )
-  //     gameOver();
-
-  //   let newHead = {
-  //     x: snakeX,
-  //     y: snakeY,
-  //   };
-  //   eatSelfTail(newHead, snake);
-  //   snake.unshift(newHead);
-  // }
-
-  // //let game = setInterval(draw, 100);
-
-  // function gameOver() {
-  //   ctx.fillStyle = "#fff";
-  //   ctx.font = "40px Monospace";
-  //   ctx.fillText(`Game over, Your score: ${score}`, 50, 200);
-  //   ctx.fillStyle = "#000";
-  //   ctx.fillText(`Game over, Your score: ${score}`, 51, 201);
-  //   clearInterval(game);
-  // }
-
-  // requestAnimationFrame(animate);
+  animate();
 });
